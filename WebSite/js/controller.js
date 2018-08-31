e8231868ac2101a82bb3702cec6e3d4de06e0e58
Avery Pierce <apierce@partstown.com>
2018-08-31 10:38:39 -0500

    Initial commit

---
+++ WebSite/js/controller.js
@@ -0,0 +1,85 @@
+angular.module('QuizApp', []).controller('QuizController', ['$http', function($http) {
+	const HOST_DOMAIN = `elginoscars.com`;
+	const COUCHDB_ROOT = `https://${HOST_DOMAIN}:6984`;
+	const QUIZ_DATABASE_NAME = `quizzes`;
+	const QUIZ_ID = `disney-double`;
+	const QUIZ_DOC_ID = `quiz:${QUIZ_ID}`;
+
+	$http.get(`${COUCHDB_ROOT}/${QUIZ_DATABASE_NAME}/${QUIZ_DOC_ID}`).then((result) => {
+		console.log(result);
+		this.description = JSON.stringify(result.data);
+		this.questions = result.data.questions;
+	})
+
+
+	// The title appears at the top of the quiz
+	this.title = "";
+	this.questions = [];
+
+	// Take the question's input, and move it to its guesses.
+	this.submitQuestion = function(question) {
+		_.defaults(question, {"guesses": []});
+		question.guesses.push(question.input);
+		question.input = "";
+	}
+
+	// Evaluate one of the question's guesses
+	this.evaluateAnswer = function(question, guess) {
+		const solutions = question.solutions;
+		const allowedResponses = _.flatMap(solutions, (solution) => {
+			return _.union([solution.canonicalName], solution.alternatives);
+		})
+		return (_.some(allowedResponses, (allowedResponse) => { return allowedResponse == guess }));
+	}
+
+	// Submit the quiz to the database so that it can be shared!
+	this.submit = function() {
+		console.log("submitting results");
+		const submissionID = "test-"+makeRandomSubmissionID();
+		const guesses = this.questions.map((question) => {
+			return question.guesses || [];
+		});
+
+		const submission = {
+			"_id": `submission:${submissionID}`,
+			"type": "submission",
+			"quiz_id": `quiz:${QUIZ_ID}`,
+			"answers": guesses
+		};
+
+		$http.put(`${COUCHDB_ROOT}/${QUIZ_DATABASE_NAME}/submission:${submissionID}`, submission).then((result) => {
+			const sharableURL = `http://${HOST_DOMAIN}/?ref=${submissionID}`;
+			this.sharableURL = sharableURL;
+		})
+	}
+
+
+	const searchParams = parseQueryString(location.search);
+	const competingAttempt = searchParams.ref;
+	if (competingAttempt) {
+		$http.get(`${COUCHDB_ROOT}/${QUIZ_DATABASE_NAME}/submission:${competingAttempt}`).then((result) => {
+			console.log(result);
+			this.competitor = result.data;
+			this.competitor.shownAnswers = {};
+		})
+	}
+
+	this.theirAnswersForQuestion = function(questionIndex) {
+		if (this.competitor) {
+			return this.competitor.answers[questionIndex];
+		} else {
+			return [];
+		}
+	}
+
+	this.showTheirAnswersButtonTitle = function() {
+		if (this.competitor.name) {
+			return `Show ${this.competitor.name}'s Answers`;
+		} else {
+			return "Show Their Answers";
+		}
+	}
+
+
+}])
+console.log("Loaded!");
---
+++ WebSite/js/util.js
@@ -0,0 +1,35 @@
+function randomAlphaNumericChar() {
+	const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const index = Math.floor(Math.random() * CHARS.length);
+	return CHARS[index];
+}
+
+function makeRandomSubmissionID() {
+	return '********'.replace(/\*/g, function() {
+		return randomAlphaNumericChar();
+	});
+}
+
+/// https://stackoverflow.com/a/979995/1422794
+function parseQueryString(query) {
+	query = _.trimStart(query, '?');
+	var vars = query.split("&");
+	var query_string = {};
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		var key = decodeURIComponent(pair[0]);
+		var value = decodeURIComponent(pair[1]);
+		// If first entry with this name
+		if (typeof query_string[key] === "undefined") {
+			query_string[key] = decodeURIComponent(value);
+			// If second entry with this name
+		} else if (typeof query_string[key] === "string") {
+			var arr = [query_string[key], decodeURIComponent(value)];
+			query_string[key] = arr;
+			// If third or later entry with this name
+		} else {
+			query_string[key].push(decodeURIComponent(value));
+		}
+	}
+	return query_string;
+}
